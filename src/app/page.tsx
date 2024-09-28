9086f70411725ecd407100f30d890da49027ef21
eshgin1 <87391720+eshgin1@users.noreply.github.com>
2024-09-29 02:17:13 +0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { Layout, Flex } from "antd";
+import Link from "next/link";
+import Category from "./components/Category";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+export default function Home() {
+  return (
+    <Flex gap="middle" wrap>
+      <Layout >
+        <Header className="bg-white flex justify-between border-b-[1px]">
+          <Link href="/">
+            <img src="/insight/my-app/src/app/assets/icons/logo.svg" alt="logo" />
+          </Link>
+          <div>
+            <a href="tel:+79528278833" className="text-[20px] text-[#48b03a] hover:text-[#48b03a]">8-(952)-827-88-33</a>
+          </div>
+          <ul className="flex gap-[20px]">
+            <li>
+              <Link href="/for-mans">Для парней</Link>
+            </li>
+            <li>
+              <Link href="/for-women">Для девушек</Link>
+            </li>
+            <li>
+              <Link href="/no-milk">Без глютена и молока</Link>
+            </li>
+          </ul>
+        </Header>
+        <Content className="bg-white pl-[30px] pr-[30px]">
+            <div className="mt-[60px] w-[560px] mx-auto">
+              <h1 className="text-center text-[50px]">Изысканная еда как стиль жизни</h1>
+              <h2 className="mt-[10px] text-center text-[24px] opacity-[0.7]">Готовое полезное питание с доставкой. Освободите время для важных дел.</h2>
+            </div>
+            <Category/>
+        </Content>
+        <Footer className="bg-white border-t-[1px] flex justify-between">
+          <Link href="/">
+            <img src="/insight/my-app/src/app/assets/icons/logo.svg" alt="logo" />
+          </Link>
+          <div>
+            <div className="flex justify-between">
+              <ul className="flex flex-col gap-[20px]">
+                <li>
+                  <Link href="/for-mans">Для парней</Link>
+                </li>
+                <li>
+                  <Link href="/for-women">Для девушек</Link>
+                </li>
+                <li>
+                  <Link href="/no-milk">Без глютена и молока</Link>
+                </li>
+              </ul>
+              <ul className="flex flex-col gap-[20px]">
+                <a target="_blank" href="https://t.me/insight_food_sochi">
+                  <img src="" alt="telegram" />
+                </a>
+                <a target="_blank" href="https://www.instagram.com/insighte.food">
+                  <img src="" alt="instagram" />
+                </a>
+                <a target="_blank" href="https://wa.me/79528278833">
+                  <img src="" alt="whatsapp" />
+                </a>
+              </ul>
+            </div>
+            <p className="text-[#5c5c5c] w-[300px] mt-[20px]">*Суд признал экстремистской и запретил в России
+            деятельность корпорации Meta по реализации социальных сетей</p>
+          </div>
+        </Footer>
+      </Layout>
+    </Flex>
+  );
+}
